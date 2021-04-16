f517e263a7542646478d9f683e3784386a5550f6
Wada-Kosuke <wkwk.0414@gmail.com>
2021-04-17 01:22:18 +0900

    Skywayページ移動まで

---
+++ plugins/constants.ts
@@ -0,0 +1,8 @@
+namespace Constants {
+  export const ROLE_CALLER = 1;             // 1:1　かける側
+  export const ROLE_RECEIVER = 2;           // 1:1　受ける側
+  export const ROLE_ROOM_CREATER = 3;       // room　作成者
+  export const ROLE_ROOM_PARTICIPANT = 4;   // room　参加者
+}
+
+export default Constants
