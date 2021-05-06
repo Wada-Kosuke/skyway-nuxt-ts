15cc9d23978db07863c4b079edb533e55c38c9ff
Wada-Kosuke <wkwk.0414@gmail.com>
2021-05-06 23:57:39 +0900

    homeレイアウト修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/destyle.css',
+    '~/assets/css/destyle.css',
     { src: '~/assets/scss/common.scss', lang: 'scss' },
   ],
 
