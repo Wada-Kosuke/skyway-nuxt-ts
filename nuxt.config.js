3c979ffed2d3204a2b61c760952a174edece75d0
koh <koh@uncode.co.jp>
2021-06-22 23:42:55 +0900

    配信形式追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,12 @@ export default {
     { src: '~/assets/scss/common.scss', lang: 'scss' },
   ],
 
+  styleResources: {
+    scss: [
+      '~/assets/scss/setting.scss',
+    ],
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/utils',
