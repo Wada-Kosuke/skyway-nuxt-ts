5da4afefc29c4b0246a0427380e75b34b3addb24
koh <koh@uncode.co.jp>
2021-06-29 23:29:42 +0900

    立場選択、名前入力部分コンポーネント化

---
+++ components/NameInput1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="name d-flex flex-column flex-md-row justify-center align-start align-md-center mb-4">
+    <div class="head">{{head}}：</div>
+    <div class="input">
+      <input @input="emitName1" :disabled="isStarted" type="text" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  model: {
+    prop: "name1",
+    event: "emit-name1"
+  },
+  props: {
+    name1: {
+      type: String
+    },
+    head: {
+      type: String
+    },
+    isStarted: {
+      type: Boolean
+    }
+  },
+  methods: {
+    emitName1(e: InputEvent) {
+      this.$emit("emit-name1", (e.target as HTMLInputElement).value);
+    }
+  }
+});
+</script>
---
+++ components/SelectRole.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="select-role mx-auto mt-12 d-flex flex-column justify-center">
+    <v-btn x-large @click="setRole(role1)" class="mb-6">{{btnText1}}</v-btn>
+    <v-btn x-large @click="setRole(role2)">{{btnText2}}</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    role1: Number,
+    role2: Number,
+    btnText1: String,
+    btnText2: String
+  },
+  methods: {
+    setRole(role: Number) {
+      this.$emit("set-role", role);
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.select-role {
+  width: 200px;
+}
+</style>
